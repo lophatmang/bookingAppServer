502d5f9db019938223a15fcb9a7713af60e29986
LoPhatMang <thanhntfx32690@funix.edu.vn>
2024-11-22 17:38:13 +0700

    Update hotel.js

--- routes/hotel.js
+++ routes/hotel.js
@@ -6,6 +6,7 @@ const hotelController = require("../controllers/hotel");
 const roomController = require("../controllers/room");
 
 router.get("/city", hotelController.GetCity);
+
 router.get("/type-hotel", hotelController.getTypeHotel);
 router.get("/top-rate", hotelController.getTopRate);
 router.get("/detail/:hotelId", hotelController.getDetaiHotel);
